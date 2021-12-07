0122ee9c725409004d7067f9136380de08a79968
daoleno <daodaoonline@gmail.com>
2021-12-07 18:08:09 +0800

    Introduce useSWR. NFTGallery will be faster.

--- pages/nftgallery.js
+++ pages/nftgallery.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
-import { fetchNFT, getErrorMessage, toHttp } from "../lib/web3";
+import { getErrorMessage, toHttp } from "../lib/web3";
 import Notify from "../components/Notify";
 import { SearchIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/dist/client/router";
 import tokensNFT from "../lib/tokensNFT";
 import Link from "next/link";
 import PopOver from "../components/Popover";
+import { useNFT, useTotalSupply } from "../lib/hooks";
 
 const emojis = [
   "🔥",
@@ -45,31 +46,16 @@ const emojis = [
 ];
 
 export default function NFTGallery() {
-  const { query, push, replace, isReady } = useRouter();
+  const { query, replace } = useRouter();
   const { search } = query;
   const [error, setError] = useState();
-  const [nfts, setNFTs] = useState([]);
-  const [searchVaule, setSearchVaule] = useState();
+  const { totalSupply, error: totalSupplyError } = useTotalSupply(search);
   const [fetchIndex, setFetchIndex] = useState(0);
 
   async function handleFetch(e) {
     e.preventDefault();
     replace(`/nftgallery?search=${searchVaule}`);
   }
-  async function fetchNFTOnPageLoad() {
-    if (!isReady || !search) {
-      return;
-    }
-    try {
-      setNFTs([]);
-      await fetchNFT({ search, setNFTs, fetchIndex, setFetchIndex });
-    } catch (err) {
-      setError(err);
-    }
-  }
-  useEffect(() => {
-    fetchNFTOnPageLoad();
-  }, [search]);
 
   return (
     <>
@@ -112,7 +98,7 @@ export default function NFTGallery() {
             </div>
           </div>
         </form>
-        {nfts.length == 0 && !search && (
+        {!search && (
           <ul
             role="list"
             className="grid grid-cols-2 gap-x-4 gap-y-5 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 mt-8 overflow-y-auto h-screen"
@@ -134,7 +120,7 @@ export default function NFTGallery() {
           </ul>
         )}
         {/* Loading Spinner */}
-        {nfts.length == 0 && search && (
+        {/* {nfts.length == 0 && search && (
           <div className="flex justify-center items-center mt-48">
             <img
               src="rocket.png"
@@ -142,58 +128,75 @@ export default function NFTGallery() {
               alt="loading"
             />
           </div>
-        )}
-        <ul
-          role="list"
-          className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
-        >
-          {nfts.map((nft, i) => (
-            <li key={i} className="relative">
-              <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-                <img
-                  src={toHttp(nft.image)}
-                  alt={nft.name}
-                  className="object-cover pointer-events-none group-hover:opacity-75"
-                />
-                <button type="button" className="absolute">
-                  <span className="sr-only">View details for {nft.name}</span>
-                </button>
-              </div>
-              <div className="flex justify-around mt-2 text-sm font-medium text-gray-900">
-                <a href={toHttp(nft.image)} target="_blank">
-                  {nft.name}
-                </a>
-                <PopOver info={nft} />
-              </div>
-              {/* <p className="block text-sm font-medium text-gray-500 pointer-events-none">
-                {nft.description}
-              </p> */}
-            </li>
-          ))}
-        </ul>
-        {nfts.length > 0 && (
+        )} */}
+        {search && <NFTCardPage address={search} index={fetchIndex} />}
+        {
           <div className="flex justify-center items-center">
             <button
               type="button"
               className="inline-flex items-center mt-10 px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={async () => {
-                try {
-                  await fetchNFT({
-                    search,
-                    setNFTs,
-                    fetchIndex,
-                    setFetchIndex,
-                  });
-                } catch (err) {
-                  setError(err);
-                }
+              onClick={(e) => {
+                e.preventDefault();
+                setFetchIndex(fetchIndex + 8);
               }}
             >
-              Load More
+              {`${totalSupply - fetchIndex}/${totalSupply}`}
             </button>
           </div>
-        )}
+        }
       </div>
+    </>
+  );
+}
+
+function NFTCardPage({ address, index = 0 }) {
+  console.log("fetchindex", index);
+  const pageLimit = 8;
+  const nfts = [];
+  for (let i = index; i < index + pageLimit; i++) {
+    nfts.push(
+      <li key={i} className="relative">
+        <NFTCard address={address} index={i} />
+      </li>
+    );
+  }
+
+  return (
+    <ul
+      role="list"
+      className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+    >
+      {nfts}
+    </ul>
+  );
+}
+
+function NFTCard({ address, index }) {
+  const { nft, error } = useNFT(address, index);
+
+  return (
+    <>
+      {error && <Notify error={getErrorMessage(error)} />}
+      {nft && (
+        <>
+          <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+            <img
+              src={toHttp(nft.image)}
+              alt={nft.name}
+              className="object-cover pointer-events-none group-hover:opacity-75"
+            />
+            <button type="button" className="absolute">
+              <span className="sr-only">View details for {nft.name}</span>
+            </button>
+          </div>
+          <div className="flex justify-around mt-2 text-sm font-medium text-gray-900">
+            <a href={toHttp(nft.image)} target="_blank">
+              {nft.name}
+            </a>
+            <PopOver info={nft} />
+          </div>
+        </>
+      )}
     </>
   );
 }
