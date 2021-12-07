5ed37e3a33819fc961bd03a78438fab2294e0060
daoleno <daodaoonline@gmail.com>
2021-12-07 13:47:06 +0800

    Add google analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,44 @@
 import "tailwindcss/tailwind.css";
 import Layout from "./layout";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <Component {...pageProps} />
     </Layout>
   );
